3172f1823d3bf6e738284b88ed23cf2d5eefb28c
S-H-GAMELINKS <gamelinks007@gmail.com>
2017-08-26 22:57:46 +0900

    選択肢描画関数：CHOICE関数の実装

--- GameBook/main.cpp
+++ GameBook/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <stdlib.h>
 
 std::vector<std::string> Script;
 
@@ -20,9 +21,37 @@ void ScriptRead(int root) {
 		Script.emplace_back(std::move(s));
 }
 
+//選択肢描画関数
+void ChoiceDraw(int root) {
+
+	std::string choice1, choice2;
+
+	const char* Choice[][2] = { {"CHOICE/A.txt", "CHOICE/B.txt"},
+								{"CHOICE/C.txt", "CHOICE/D.txt" },
+								{"CHOICE/E.txt","CHOICE/F.txt"} };
+
+	std::fstream File1(Choice[root - 1][0]);
+	std::fstream File2(Choice[root - 1][1]);
+
+	std::getline(File1, choice1);
+	std::getline(File2, choice2);
+
+	std::cout << "1 : " << choice1 << std::endl;
+	std::cout << "2 : " << choice2 << std::endl;
+}
+
 //スクリプト描画関数
-void ScriptDraw() {
+void ScriptDraw(int root) {
+
+	int RootChoice;
+
 	for (auto&& s : Script) {
+
+		if (s == "CHOICE") {
+			system("cls");
+			ChoiceDraw(root);
+			std::cin >> RootChoice;
+		}
 
 		if (s == "END")
 			break;
@@ -38,7 +67,7 @@ int main() {
 
 	ScriptRead(root);
 
-	ScriptDraw();
+	ScriptDraw(root);
 
 	return 0;
 }
