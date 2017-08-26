f6b26a24d160ad05a7863315d1629604f6bf5842
S-H-GAMELINKS <gamelinks007@gmail.com>
2017-08-26 21:20:17 +0900

    スクリプトファイルの読込と描画を実装

--- GameBook/main.cpp
+++ GameBook/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+std::vector<std::string> Script;
 
 int main() {
+
+	std::fstream File("test.txt");
+
+	for (std::string s; std::getline(File, s); )
+		Script.emplace_back(std::move(s));
+
+	for (auto&& s : Script) {
+		std::cout << s << std::endl;
+		getchar();
+	}
 
 	return 0;
 }
